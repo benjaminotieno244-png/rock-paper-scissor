94faacc38f09d9aa177d101639922a3354c4ec94
benjaminotieno <258400609+benjaminotieno244-png@users.noreply.github.com>
2026-07-02 14:30:36 +0300

    Add playRound function with score tracking

--- script.js
+++ script.js
@@ -13,5 +13,29 @@ function getComputerChoice() {
     } else {
         return "scissors";
     }
-}
+}let humanscore = 0;
+ let computerscore = 0;  
 console.log(getComputerChoice());
+//ask a player to type in their choice of rock, paper, or scissors
+//return whatever the player types in
+function getHumanChoice() {
+    const choice = prompt("Enter rock, paper, or scissors:");
+    return choice;
+}
+console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!"); humanscore++
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log("You win!"); humanscore++
+    } else {
+        console.log("You lose!"); computerscore++
+    }
+}
+console.log(playRound(getHumanChoice(), getComputerChoice()));
